1549587047957b1669fd67c7b998ff797b686174
Chris Clower <chris@clowerweb.com>
2016-04-11 10:47:25 -0400

    Update intersections.lua

--- intersections.lua
+++ intersections.lua
@@ -2,7 +2,9 @@
 -- useful for running logic only
 -- when an object is at an intersection
 -- to preserve CPU cycles
--- by Scathe (@clowerweb)
+
+-- Contributors: Scathe (@clowerweb)
+
 function int(a)
  return (flr(a.x)%8==0 and
          flr(a.y)%8==0)
