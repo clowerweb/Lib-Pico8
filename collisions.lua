c0e04ac389f0909a9b57c416bec4a7bddebab9b0
Chris Clower <chris@clowerweb.com>
2016-04-11 09:44:52 -0400

    Create collisions.lua

---
+++ collisions.lua
@@ -0,0 +1,40 @@
+
+-- collisions with map tiles
+function coll_map(o)
+  local x1=o.x/8
+  local y1=o.y/8
+  local x2=(o.x+7)/8
+  local y2=(o.y+7)/8
+  local a=fget(mget(x1,y1),0)
+  local b=fget(mget(x1,y2),0)
+  local c=fget(mget(x2,y2),0)
+  local d=fget(mget(x2,y1),0)
+  return a or b or c or d
+end
+
+-- collision with world bounds
+function coll_world(o,h,v)
+  if(h and o.x<0 or o.x+o.width>w) then
+    return true
+  end
+  if(v and o.y<0 or o.y+o.height>h) then
+    return true
+  end
+end
+
+-- collision with objects
+function coll_obj(o1,o2)
+ for k in pairs(o2) do
+  if((a.x<=b[k].x+b[k].width) and
+     (a.x+a.width>=b[k].x) and
+     (a.y<=b[k].y+b[k].height) and
+     (a.y+a.height>=b[k].y)) then
+   -- returns the index of the object 
+   -- you are colliding with
+   return k
+  end
+ end
+end
+
+-- pixel perfect collision
+-- coming soon!
