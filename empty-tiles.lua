14e9daecf5c95929b91c71b6dd620aef0bb9604b
Chris Clower <chris@clowerweb.com>
2016-04-11 10:24:52 -0400

    Create empty-tiles.lua

---
+++ empty-tiles.lua
@@ -0,0 +1,22 @@
+-- makes a list of empty tiles
+function empty_tiles(f)
+ local cols=(w-8)/8
+ local rows=(h-8)/8
+ local tiles={}
+
+ for y=0,rows do
+  for x=0,cols do
+   if(not fget(mget(x+ox/8,y+oy/8),0)) then
+    if(not f) then
+     add(tiles,{x*8,y*8})
+    else -- away from player
+     if(dst(x*8,curp.x,y*8,curp.y)>=40) then
+      add(tiles,{x*8,y*8})
+     end
+    end
+   end
+  end
+ end
+
+ return tiles
+end
