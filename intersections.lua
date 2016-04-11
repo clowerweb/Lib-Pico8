59cfc7b69c63fb3ab879fcef8401b72d2877fb48
Chris Clower <chris@clowerweb.com>
2016-04-11 10:32:53 -0400

    Update intersections.lua

--- intersections.lua
+++ intersections.lua
@@ -1,4 +1,8 @@
 -- check if at intersection
+-- useful for running logic only
+-- when an object is at an intersection
+-- to preserve CPU cycles
+-- by Scathe (@clowerweb)
 function int(a)
  return (flr(a.x)%8==0 and
          flr(a.y)%8==0)
