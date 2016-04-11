076f4a569ea9493e6f3e16c1b4e31abddc8a13fd
Chris Clower <chris@clowerweb.com>
2016-04-11 09:30:53 -0400

    Update animation.lua

--- animation.lua
+++ animation.lua
@@ -3,7 +3,7 @@ function anim(a,anim,offx,offy)
 		anim.loop=true
 	end
 	anim.step=anim.step or 0
-
+	
 	local sta=anim.start
 	local cur=anim.current or 0
 	local stp=anim.step
@@ -13,7 +13,7 @@ function anim(a,anim,offx,offy)
 	local rst=anim.reset or 0
 	local lop=anim.loop
 	local rev=anim.reverse or false
-
+	
 	anim.step+=1
 	stp=anim.step
 	if(not rev) then
@@ -26,17 +26,17 @@ function anim(a,anim,offx,offy)
 		if(stp%flr(30/spd)==0)	cur-=1
 		if(cur<0) cur=0
 	end
-
+	
 	if(a.md and not a.mv and
 	not anim.imv) then
 		cur=rst
 	end
-
+	
 	anim.current=cur
 	a.anim.play=anim
-
+	
 	if(not offx) offx=0
 	if(not offy) offy=0
-
+	
 	spr(sta+cur,a.x+offx,a.y+offy,1,1,flx,fly)
 end
