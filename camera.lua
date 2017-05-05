5eeffc4c6774d9ddb553bab24e4da1ff19036257
Matthew Sauder <matthewsauder@gmail.com>
2017-05-05 10:36:56 -0600

    Added camera shake

--- camera.lua
+++ camera.lua
@@ -2,5 +2,25 @@
 -- will contain functions for
 -- manipulating the camera,
 -- such as following the player
--- with smooth scrolling, and
--- effects such as camera shaking
+-- with smooth scrolling
+
+--methods originally by @trasevol_dog
+--transcribed/editted by @msauder
+
+--shkx,shky need to be initialized in _init()
+--call camera(shkx,shky) at start of _draw()
+--and be sure to update_shake() in _update()
+
+function add_shake(p)
+  local a=rnd(1)
+  shkx=p*cos(a)
+  shky=p*sin(a)
+end
+
+function update_shake()
+  if abs(shkx)+abs(shky)<0.5 then
+    shkx,shky=0,0
+  end
+  shkx*=-0.7-rnd(2)
+  shky*=-0.7-rnd(2)
+end
