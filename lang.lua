00b4436455354c056d6349a95e4fa609abe04edd
sulai <sulai@gmx.de>
2017-09-04 13:24:20 +0200

    added idr, idstart, idend for convenient access

--- lang.lua
+++ lang.lua
@@ -53,13 +53,19 @@ function enum(names, offset)
 	offset=offset or 1
 	local objects = {}
 	local size=0
-	for id,name in pairs(names) do
-		id = id + offset - 1
-		local obj = {id=id, name=name}
+	for idr,name in pairs(names) do
+		local id = idr + offset - 1
+		local obj = {
+			id=id,       -- id
+			idr=idr,     -- 1-based relative id, without offset being added
+			name=name    -- name of the object
+		}
 		objects[name] = obj
 		objects[id] = obj
 		size=size+1
 	end
+	objects.idstart = offset        -- start of the id range being used
+	objects.idend = offset+size-1   -- end of the id range being used
 	objects.size=size
 	objects.all = function()
 		local list = {}
